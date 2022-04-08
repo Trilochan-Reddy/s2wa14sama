5f1bdecdb916da7ea1e2d865b1f202a4c02bb001
Trilochan-Reddy <trilochanreddy100@gmail.com>
2022-04-08 10:15:04 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,39 +1,32 @@
 var express = require('express');
-
 var router = express.Router();
 
-var storeValue;
+/* GET computation page. */
+router.get('/', function (req, res, next) {
+  var x;
+  //var random = Math.random() * (3) + (-1);
+  var random = Math.random();
+  console.log(req.query.x);
+  x = req.query.x;
 
-/* GET home page. */
-
-router.get('/', function(req, res, next) {
-
-    fake_url = "https://fake.com/path" + req.url
-
-      const url = new URL(fake_url)
-
-      const search_params = url.searchParams
-
-
-
-   
-
-      storeValue=Number(search_params.get("x"))
-
-    // if paramter is not specified  value of Number(search_params.get("x")) is always zero
-
-    //console.log(Number(search_params.get("x"))+"hello")
-
-    if(storeValue == 0)
-
-    storeValue=Math.random()
-
-    res.write('Math.atanh applied to '+storeValue+" is "+Math.asin(storeValue)+"\n");
-
-    res.end('Math.sign applied to '+storeValue+" is "+Math.cos(storeValue));
-
- });
+  // checking if url has params
+  if (x == undefined) {
+    x = random;
+  }
 
 
+  
+  let cos = Math.cos(x,x) 
+  let asin =Math.asin(x)
+  let asinh = Math.asinh(x)
+  
+  res.render('computation', {
+    title: 'Computation',
+    values: `applied to ` + x + ` is ` + cos ,
+    v1: `applied to ` + x + ` is ` + asin,
+    v2: `applied to ` + x + ` is ` + asinh,
+    
+  });
+});
 
 module.exports = router;
